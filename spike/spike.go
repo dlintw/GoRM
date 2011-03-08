4c200076cb5aea3636c51f91e26730aee854d69e
Steven Degutis <steven@degutis.org>
2011-03-08 08:54:50 -0600

    cool spike

--- spike/spike.go
+++ spike/spike.go
@@ -2,17 +2,43 @@ package main
 
 import (
 	"log"
+	"fmt"
 	"gosqlite.googlecode.com/hg/sqlite"
 )
 
 func main() {
-	conn, err := sqlite.Open("test.go")
+	conn, err := sqlite.Open("test.db")
+	defer conn.Close()
 	
 	if err != nil {
 		log.Fatal(err)
 	}
 	
-	println(conn)
+    s, err := conn.Prepare("select * from persons")
+    if err != nil {
+            log.Fatal(err)
+    }
+    defer s.Finalize()
+    err = s.Exec()
+    if err != nil {
+        log.Fatal(err)
+    }
 	
-	conn.Close()
+	for s.Next() {
+		fields := make([]interface{}, 3)
+		
+		for i, _ := range fields {
+			var s string
+			fields[i] = &s
+		}
+		
+		s.Scan(fields...)
+		// fields is now a slice of pointers to strings
+		
+		for _, v := range fields {
+			if v, ok := v.(*string); ok {
+				fmt.Printf("%v\n", *v)
+			}
+		}
+	}
 }
