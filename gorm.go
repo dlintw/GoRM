8332a7eb36f366fbe83b1da975ab24a1496108e1
Steven Degutis <steven@degutis.org>
2011-03-12 10:54:29 -0600

    adding get api and almost finished

--- gorm.go
+++ gorm.go
@@ -3,6 +3,10 @@ package gorm
 import (
 	"os"
 	"sdegutis/sqlite"
+	"fmt"
+	
+	// testing
+	"log"
 )
 
 type Conn struct {
@@ -16,4 +20,43 @@ func (c *Conn) Close() os.Error {
 func OpenDB(filename string) (*Conn, os.Error) {
 	conn, err := sqlite.Open("test.db")
 	return &Conn{conn: conn}, err
+}
+
+func (c *Conn) Get(rowStruct interface{}, condition string, args ...interface{}) os.Error {
+	tname, _ := getTypeName(rowStruct)
+	tname = snakeCasedName(tname)
+	tableName := pluralizeString(tname)
+	
+	condition, err := escapeString(condition, args...)
+	if err != nil {
+		return err
+	}
+	
+	condition = fmt.Sprintf("where %v", condition)
+	
+    s, err := c.conn.Prepare(fmt.Sprintf("select * from %v %v", tableName, condition))
+    if err != nil {
+            log.Fatal(err)
+    }
+    defer s.Finalize()
+    err = s.Exec()
+    if err != nil {
+        log.Fatal(err)
+    }
+	
+	if s.Next() {
+		results, err := s.ResultsAsMap()
+		
+		log.Fatal(results)
+		
+		fmt.Println(results, err)
+	} else {
+		return os.NewError("did not find any results")
+	}
+	
+	if s.Next() {
+		return os.NewError("more than one row matched")
+	}
+	
+	return nil
 }
--- gorm_test.go
+++ gorm_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Id int
+	Name string
+	Age int
+}
+
 func TestOpenDB(t *testing.T) {
 	db, err := OpenDB("test.db")
 	if err != nil || db == nil {
@@ -12,5 +18,24 @@ func TestOpenDB(t *testing.T) {
 	err = db.Close()
 	if err != nil {
 		t.Errorf("closing db connection should not have failed either")
+	}
+}
+
+func TestGetSingle(t *testing.T) {
+	return // gotta test scanning map into struct first
+	
+	db, err := OpenDB("test.db")
+	defer db.Close()
+	
+	bob := Person{}
+	
+	err = db.Get(&bob, "name = ?", "bob")
+	
+	if err != nil {
+		t.Error(err)
+	}
+	
+	if bob.Name != "bob" || bob.Age != 42 || bob.Id != 2 {
+		t.Errorf("bob was not filled out properly [%v]", bob)
 	}
 }
